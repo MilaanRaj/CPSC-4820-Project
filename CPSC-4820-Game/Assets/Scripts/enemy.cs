750ae36c66336ef54db859cd4ef3e7b08d3d0cb9
MilaanRaj <mraj@g.clemson.edu>
2024-10-29 13:19:12 -0400

    Waypoints for enemy AI created
    We have movements.

---
+++ CPSC-4820-Game/Assets/Scripts/enemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position,target.position)<= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointindex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointindex++;
+        target = Waypoints.points[wavepointindex];
+    }
+}
