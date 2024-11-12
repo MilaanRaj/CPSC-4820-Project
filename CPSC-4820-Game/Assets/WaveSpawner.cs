f498e645b8c5af676b5432e30f4e4880c2653ff6
mraj <mraj@g.clemson.edu>
2024-11-11 22:32:34 -0500

    WaveSpawner(multiple enemies)

---
+++ CPSC-4820-Game/Assets/WaveSpawner.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+
+    private int waveNumber = 1;
+
+
+
+    private void Update()
+    {
+        if(countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave Incoming!");
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+
+        }
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
