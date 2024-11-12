af8585900406eb232e8ca3b2b4cc77e9204e1f19
mraj <mraj@g.clemson.edu>
2024-11-12 14:16:27 -0500

    Turret
    Turning still doesn't work but it will lock on to enemies

---
+++ CPSC-4820-Game/Assets/Scripts/Turret.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class Turret : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("updateTarget", 0f, 0.5f);
+    }
+
+    void updateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        Vector3 rotation = lookRotation.eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
