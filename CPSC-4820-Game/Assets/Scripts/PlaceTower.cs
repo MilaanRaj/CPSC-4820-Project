2c8dec8f51f713536d1bc591f9c5d25b0fc1e0fa
phoenixsrash <psrash19@gmail.com>
2024-11-13 01:16:37 -0500

    start of a script for placing towers

--- CPSC-4820-Game/Assets/Scripts/PlaceTower.cs
+++ CPSC-4820-Game/Assets/Scripts/PlaceTower.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlaceTower : MonoBehaviour
+public class TowerPlacementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] towerPrefabs;  // Array to hold different tower prefabs
+    public GameObject selectedTowerPrefab;  // The currently selected tower to place
+
     void Start()
     {
-        
+        selectedTowerPrefab = null;
     }
 
-    // Update is called once per frame
+    // Method to assign selected tower from UI buttons
+    public void SelectTower(int towerIndex)
+    {
+        selectedTowerPrefab = towerPrefabs[towerIndex];
+    }
+
     void Update()
     {
-        
+        // Check for mouse click to place tower if a tower is selected
+        if (Input.GetMouseButtonDown(0) && selectedTowerPrefab != null)
+        {
+            // Get mouse position and convert to world space
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Check if the ray hits a valid placement spot
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("PlacementSpot"))
+                {
+                    // Place the selected tower on the hit spot
+                    Instantiate(selectedTowerPrefab, hit.collider.transform.position, Quaternion.identity);
+                    selectedTowerPrefab = null;  // Reset selected tower after placement
+                }
+            }
+        }
     }
 }
