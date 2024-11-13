57eac37648b549b775d76ff534f296b1a6923acc
mraj <mraj@g.clemson.edu>
2024-11-12 22:20:07 -0500

    Creation of turrets

--- CPSC-4820-Game/Assets/Scripts/Turret.cs
+++ CPSC-4820-Game/Assets/Scripts/Turret.cs
@@ -5,11 +5,20 @@ using System.Collections;
 public class Turret : MonoBehaviour
 {
     public Transform target;
+
+    [Header("Attributes")]
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
     public float range = 15f;
 
+    [Header("Unity Setup Feilds")]
     public string enemyTag = "Enemy";
-
     public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
 
     // Start is called before the first frame update
     void Start()
@@ -54,8 +63,28 @@ public class Turret : MonoBehaviour
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
 
-        Vector3 rotation = lookRotation.eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+        GameObject bulletGo = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected()
