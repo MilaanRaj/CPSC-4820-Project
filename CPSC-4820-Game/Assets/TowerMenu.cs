439d498c4f5502ff0558c5bef9fe7da10d6bcada
phoenixsrash <psrash19@gmail.com>
2024-11-13 01:15:59 -0500

    fixing in game menu + some enhancements

---
+++ CPSC-4820-Game/Assets/TowerMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TowerMenu : MonoBehaviour
+{
+    public void SelectCommanderClownfish()
+    {
+        Debug.Log("Commander Clownfish Selected");
+    }
+
+    public void SelectAnotherClown()
+    {
+        Debug.Log("Another Clownfish Selected");
+    }
+}
