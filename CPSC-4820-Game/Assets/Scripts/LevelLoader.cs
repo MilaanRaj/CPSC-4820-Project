526ad4c4cf77df9fa654a646af917683ecddfcc9
phoenixsrash <psrash19@gmail.com>
2024-11-29 22:55:20 -0500

    trying to fix loading screen

--- CPSC-4820-Game/Assets/Scripts/LevelLoader.cs
+++ CPSC-4820-Game/Assets/Scripts/LevelLoader.cs
@@ -1,58 +1,35 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LevelLoader : MonoBehaviour
+public class LoadingScene : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider slider;
     public TextMeshProUGUI progressText;
 
     public float FillSpeed = 0.5f;
-    private float targetProgress = 0f;
 
-    public void LoadLevel(int sceneIndex)
+    public void LoadScene(int sceneId)
     {
-        StartCoroutine(LoadAsynchronously(sceneIndex));
+        StartCoroutine(LoadSceneAsync(sceneId));
     }
 
-    IEnumerator LoadAsynchronously(int sceneIndex)
+    IEnumerator LoadSceneAsync(int sceneId)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-        operation.allowSceneActivation = false;  // Prevents the scene from activating until loading is complete
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
 
         loadingScreen.SetActive(true);
-        slider.value = 0f;
 
         while (!operation.isDone)
         {
-            // Update the target progress based on loading progress
-            targetProgress = operation.progress;
+            float progressText = Mathf.Clamp01(operation.progress / 0.9f);
 
-            // Smoothly increase the slider value towards targetProgress
-            slider.value = Mathf.MoveTowards(slider.value, targetProgress, FillSpeed * Time.deltaTime);
-
-            // Update the progress text to reflect the slider's current value
-            progressText.text = Mathf.RoundToInt(slider.value * 100f) + "%";
-
-            // If loading is done, activate the scene
-            if (operation.progress >= 0.9f)
-            {
-                targetProgress = 1f; // Fully load the slider
-                slider.value = Mathf.MoveTowards(slider.value, targetProgress, FillSpeed * Time.deltaTime);
-
-                // Update text when slider reaches 100%
-                progressText.text = Mathf.RoundToInt(slider.value * 100f) + "%";
-
-                // Wait until the slider is fully filled
-                if (slider.value >= 0.9f)
-                {
-                    operation.allowSceneActivation = true;
-                }
-            }
-
+            slider.value = progressText;
+            
             yield return null;
         }
     }
